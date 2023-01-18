836efb024cd2bbea4353742bb38b213fba5b7331
Dzemaaa <122994143+Dzemaaa@users.noreply.github.com>
2023-01-18 18:54:18 +0400

    test

---
+++ massivi dlya debilov/Program.cs
@@ -0,0 +1,23 @@
+﻿Random r = new Random();
+int[] cat = new int[r.Next(2, 100)];
+
+for (int i = 0; i < cat.Length; i++)
+{
+    cat[i] = r.Next(0, 10);
+}
+int count = yachmo(cat, 5);
+Console.WriteLine("в случайном массиве " + count + " четных чисел!!!");
+
+int yachmo(int[] ints, int x)
+{
+    List<int> list = new List<int>();
+
+    foreach (var item in ints)
+    {
+        if (item % 2 == 0)
+            list.Add(item);
+
+    }
+
+    return list.Count; 
+}
